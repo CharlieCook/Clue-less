aa34bde10903d62e3ed764d81fcf4fe519b47517
= <=>
2014-07-21 18:48:08 -0400

    Adding blank controller methods

--- grails-app/controllers/clue/less/CluelessController.groovy
+++ grails-app/controllers/clue/less/CluelessController.groovy
@@ -3,4 +3,16 @@ package clue.less
 class CluelessController {
 
     def index() { }
+	
+	def createGame(){}
+	
+	def gameState(UUID){}
+	
+	def move(UUID, player, location){}
+	
+	def suggest(UUID, player, suspect, location, weapon){}
+	
+	def disprove(UUID, player, card) {}
+	
+	def accuse(UUID, player, suspect, location, weapon){}
 }
