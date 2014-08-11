3a72c864bfa8d730c56a5b5e1c9d5e75205de909
Kyle <traverskyle9@gmail.com>
2014-08-10 22:54:25 -0400

    Removing unit test from GameEngine as the functionality has been moved to the location class.

--- test/unit/clue/less/GameEngineServiceSpec.groovy
+++ test/unit/clue/less/GameEngineServiceSpec.groovy
@@ -15,20 +15,7 @@ class GameEngineServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test corner office detect"() {
-		when:
-			GameEngineService service = new GameEngineService()
+    void "test gameEngine logic"() {
 		
-			Location conservatory =Location.CONSERVATORY
-			Location kitchen = Location.KITCHEN
-			Location lounge =Location.LOUNGE
-			Location study = Location.STUDY
-			Location hall = Location.HALL
-		then:
-			assertTrue("Conservatory should be a corner room", service.isCornerRoom(conservatory))
-			assertTrue("Kitchen should be a corner room", service.isCornerRoom(kitchen))
-			assertTrue("Lounge should be a corner room", service.isCornerRoom(lounge))
-			assertTrue("Study should be a corner room", service.isCornerRoom(study))
-			assertFalse("The hall is not a corner room", service.isCornerRoom(hall))
     }
 }
